ae31d346704b23108f62291405e3e19a403f8702
Denis Chernyak <chernyak.ds@gmail.com>
2024-04-15 20:58:02 +0300

    shop now fixed

--- src/js/header-anim.js
+++ src/js/header-anim.js
@@ -1,6 +1,6 @@
 function headerAnimatedto() {
   window.addEventListener('scroll', function () {
-    const sectionhowitworks = document.querySelector('.how-it-works');
+    const sectionhowitworks = document.querySelector('#how_it_works');
     const header = document.querySelector('.page-header');
     const sectionPosition = sectionhowitworks.getBoundingClientRect().top;
     if (sectionPosition < 0 && !header.classList.contains('.header-sticked')) {
