6835529c80488ceddd8854a5d79814e9e03ecf8f
Denis Chernyak <chernyak.ds@gmail.com>
2024-04-15 21:49:55 +0300

    reviews fixed

--- src/js/header-anim.js
+++ src/js/header-anim.js
@@ -3,11 +3,11 @@ function headerAnimatedto() {
     const sectionhowitworks = document.querySelector('#how_it_works');
     const header = document.querySelector('.page-header');
     const sectionPosition = sectionhowitworks.getBoundingClientRect().top;
-    if (sectionPosition < 0 && !header.classList.contains('.header-sticked')) {
-      header.classList.add('.header-sticked');
+    if (sectionPosition < 0 && !header.classList.contains('.header-sticky')) {
+      header.classList.add('.header-sticky');
     }
-    if (sectionPosition > 0 && header.classList.contains('.header-sticked')) {
-      header.classList.remove('.header-sticked');
+    if (sectionPosition > 0 && header.classList.contains('.header-sticky')) {
+      header.classList.remove('.header-sticky');
     }
   });
 }
